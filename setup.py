7c6cd61cd36797693519e15e728b07563f920e10
Dip <dippatel256@gmail.com>
2024-03-07 21:56:30 +0530

    Modified setup.py based on project

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "<VERSION_NO>"
-REPO_NAME = "<YOUR_REPO_NAME>"
-AUTHOR_USER_NAME = "<YOUR_USER_NAME>"
-SRC_REPO = "<YOUR_REPO_NAME"
-AUTHOR_EMAIL = "<YOUR_EMAIL_ID>"
+__version__ = "0.0.0"
+REPO_NAME = "finease"
+AUTHOR_USER_NAME = "dipdaiict"
+SRC_REPO = "finease"
+AUTHOR_EMAIL = "dippatel256@gmail.com"
 
 setuptools.setup(
     # Package name
@@ -19,7 +19,7 @@ setuptools.setup(
     # Author email
     author_email=AUTHOR_EMAIL,
     # Short package description
-    description="<PROJECT_DESCRIPTION>",
+    description="FinEase is a Python package for financial calculations and analysis.",
     # Detailed package description (can be README contents)
     long_description=long_description,
     # Specify the type of long description (Markdown format)
